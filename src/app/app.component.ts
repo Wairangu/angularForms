0d664a1ea62da0443dde1b9becaf4167231197ea
Isaac Wairangu <wairangui@gmail.com>
2019-05-18 16:32:50 +0300

    Added two way binding to the select element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Forms';
+  defaultQuestion: string = 'teacher';
+  answer: string = '';
 
   //using local ref
   // onSubmit(form: NgForm) {
